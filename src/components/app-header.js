2fa519c481c843ee4ad7bb4fdaffa2146bc4ecff
Ruslan-Gal <rhgalyamov@gmail.com>
2021-06-28 15:45:32 +0300

    change app-header

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './app-header.css';
 
 const AppHeader = () => {
-	return <h1>My Todo List</h1>;
+  return (
+    <div className="app-header d-flex">
+      <h1>My Todo List</h1>;
+    </div>
+  );
 };
 
 export default AppHeader;
