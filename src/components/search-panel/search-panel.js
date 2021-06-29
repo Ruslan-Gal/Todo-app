7afb1ab381bcf4c5fb2974427b691558663b34ef
Ruslan-Gal <rhgalyamov.com>
2021-06-29 21:37:02 +0300

    changed project structure

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -3,11 +3,13 @@ import './search-panel.css';
 
 const SearchPanel = () => {
 
-	return <input
-		type='text'
-		placeholder='type to search'
-		className='form-control search-input'
-	/>
+	return (
+		<input
+			type='text'
+			className='form-control search-input'
+			placeholder='type to search'
+		/>
+	)
 };
 
 export default SearchPanel;
