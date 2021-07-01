b51d53d297e2fd0dca32515031477f1dd168a38d
Ruslan-Gal <rhgalyamov.com>
2021-07-01 22:29:32 +0300

    add state to TodoListItem

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -2,14 +2,20 @@ import React from 'react';
 
 import './item-status-filter.css';
 
-const ItemStatusFilter = () => {
+export default class ItemStatusFilter extends React.Component {
+  render() {
     return (
-        <div className='btn-group'>
-            <button type='button' className='btn btn-info'>All</button>
-            <button type='button' className='btn btn-outline-secondary'>All</button>
-            <button type='button' className='btn btn-outline-secondary'>Done</button>
-        </div>
-    )
-};
-
-export default ItemStatusFilter;
+      <div className='btn-group'>
+        <button type='button' className='btn btn-info'>
+          All
+        </button>
+        <button type='button' className='btn btn-outline-secondary'>
+          All
+        </button>
+        <button type='button' className='btn btn-outline-secondary'>
+          Done
+        </button>
+      </div>
+    );
+  }
+}
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -1,29 +1,55 @@
 import React from 'react';
 import './todo-list-item.css';
 
-const TodoListItem = ({ label, important = false }) => {
+export default class TodoListItem extends React.Component {
+  state = {
+    done: false,
+  };
 
-	const style = {
-		color: important ? 'tomato' : 'black',
-		fontWeight: important ? 'bold' : 'normal'
-	};
+  onLabelClick = () => {
+    this.setState({
+      done: true,
+    });
+  };
 
-	return (
-		<span className='todo-list-item' >
-			<span className='todo-list-item-label' style={style}>
-				{label}
-			</span>
+  render() {
+    const { label, important = false } = this.props;
+    const { done } = this.state;
 
-			<button type='button' className='btn btn-outline-success btn-sm float-right'>
-				<i className='fa fa-exclamation' />
-			</button>
+    let classNames = 'todo-list-item';
+    if (done) {
+      classNames += ' done';
+    }
 
-			<button type='button' className='btn btn-outline-danger btn-sm float-right'>
-				<i className='fa fa-trash-o' />
-			</button>
-		</span>
-	)
-};
+    const style = {
+      color: important ? 'tomato' : 'black',
+      fontWeight: important ? 'bold' : 'normal',
+    };
 
-export default TodoListItem;
+    return (
+      <span className={classNames}>
+        <span
+          className='todo-list-item-label'
+          style={style}
+          onClick={this.onLabelClick}
+        >
+          {label}
+        </span>
 
+        <button
+          type='button'
+          className='btn btn-outline-success btn-sm float-right'
+        >
+          <i className='fa fa-exclamation' />
+        </button>
+
+        <button
+          type='button'
+          className='btn btn-outline-danger btn-sm float-right'
+        >
+          <i className='fa fa-trash-o' />
+        </button>
+      </span>
+    );
+  }
+}
