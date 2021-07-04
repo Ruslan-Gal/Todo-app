9831a3ea7cb7eb545a3c990611ef199a37c44077
Ruslan <rhgalyamov@gmail.com>
2021-07-05 00:44:44 +0300

    refactor code, change bootstrapcdn link

--- src/components/AppHeader/index.js
+++ src/components/AppHeader/index.js
@@ -4,7 +4,7 @@ import './style.css';
 const AppHeader = ({ toDo, done }) => {
   return (
     <div className='app-header d-flex'>
-      <h1>My Todo List</h1>;
+      <h1>Todo List</h1>
       <h2>
         {toDo} more to do, {done} done
       </h2>
--- src/components/ItemAddForm/index.js
+++ src/components/ItemAddForm/index.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './style.css';
 
 export default class ItemAddForm extends Component {
