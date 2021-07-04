f7ce0c3f8ee8a4af1f41d3fa451678cd92c4eca3
Ruslan <rhgalyamov@gmail.com>
2021-07-04 11:40:51 +0300

    items.filter search in App

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,7 +17,7 @@ export default class App extends Component {
       this.createTodoItem('Make Awesome App'),
       this.createTodoItem('Have a lunch'),
     ],
-    search: '',
+    term: '',
   };
 
   createTodoItem(label) {
@@ -79,18 +79,23 @@ export default class App extends Component {
     });
   };
 
-  onSearchLabel = (e) => {
-    console.log(e.target.value);
-    this.setState(({ search }) => {
-      return {
-        search: e.target.value,
-      };
+  search = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(term.toLowerCase()) > -1;
     });
   };
 
+  onSearchChange = (term) => {
+    this.setState({ term });
+  };
+
   render() {
-    const { todoData } = this.state;
-    const { search } = this.state;
+    const { todoData, term } = this.state;
+
+    const visibleItems = this.search(todoData, term);
 
     const doneCount = todoData.filter((el) => el.done).length;
     const todoCount = todoData.length - doneCount;
@@ -99,13 +104,12 @@ export default class App extends Component {
       <div className='todo-app'>
         <AppHeader toDo={todoCount} done={doneCount} />
         <div className='top-panel d-flex'>
-          <SearchPanel onSearchLabel={this.onSearchLabel} value={search} />
+          <SearchPanel onSearchChange={this.onSearchChange} />
           <ItemStatusFilter />
         </div>
 
         <TodoList
-          search={search}
-          todos={todoData}
+          todos={visibleItems}
           onDeleted={this.deleteItem}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './search-panel.css';
 
-const SearchPanel = ({ onSearchLabel, value }) => {
-  return (
-    <input
-      type='text'
-      className='form-control search-input'
-      placeholder='type to search'
-      onChange={onSearchLabel}
-      value={value}
-    />
-  );
-};
+export default class SearchPanel extends Component {
+  state = {
+    term: '',
+  };
 
-export default SearchPanel;
+  onSearchChange = (e) => {
+    const term = e.target.value;
+    this.setState({ term });
+    this.props.onSearchChange(term);
+  };
+
+  render() {
+    return (
+      <input
+        type='text'
+        className='form-control search-input'
+        placeholder='type to search'
+        value={this.state.term}
+        onChange={this.onSearchChange}
+      />
+    );
+  }
+}
